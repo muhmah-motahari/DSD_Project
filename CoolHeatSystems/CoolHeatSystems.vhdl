190a7d2ed6ed15dc0923c7a16d570354605d4ac4
Muhammad Mahdi Motahari <mm.motahari.2001@gmail.com>
2023-07-24 05:48:17 +0330

    Add CoolHeatSystems module ENTITY

---
+++ CoolHeatSystems/CoolHeatSystems.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity CoolHeatSystems is
+	port(chs_conf	:	in	std_logic_vector(7 downto 0);
+			chs_power	:	out	std_logic_vector(3 downto 0);
+			chs_mode	:	out	std_logic);
+end entity;
+
+architecture struct of CoolHeatSystems is
+
+begin 
+
+end architecture;
