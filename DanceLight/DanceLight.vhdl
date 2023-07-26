3cb87270e23141f5509bd499f87c61cae61c4617
Muhammad Mahdi Motahari <mm.motahari.2001@gmail.com>
2023-07-26 11:41:47 +0330

    Add DanceLight (module 5) but empty:)))

---
+++ DanceLight/DanceLight.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity DanceLight is
+	port(clk,	load,	din	:	in	std_logic;
+			p	:	in	std_logic_vector(7 downto 0);
+			q	:	out	std_logic_vector(7 downto 0));
+end entity;
+
+architecture struct of DanceLight is
+
+begin
+
+end architecture;
