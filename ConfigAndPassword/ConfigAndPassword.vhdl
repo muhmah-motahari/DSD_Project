06e8ae1df6cf0765990e0b2bf292a650764ada27
Hami <78771728+MHmi1@users.noreply.github.com>
2023-08-07 21:49:20 +0330

    comments added

--- ConfigAndPassword/ConfigAndPassword.vhdl
+++ ConfigAndPassword/ConfigAndPassword.vhdl
@@ -5,23 +5,23 @@ use ieee.std_logic_1164.all;
 entity ConfigAndPassword is
 	port(request : in std_logic;
 			confirm : in std_logic;
-			pass	:	in	std_logic_vector(1 downto 0);
-			memIn	:	in	std_logic_vector(34 downto 0);
-			memSel	:	in	std_logic_vector(2 downto 0);
+			pass	:	in	std_logic_vector(1 downto 0);  --two bit input password 
+			memIn	:	in	std_logic_vector(34 downto 0);  --set memory content
+			memSel	:	in	std_logic_vector(2 downto 0);  --sel memory 
 			clk	:	in	std_logic;
-			memOut	:	out	std_logic_vector(34 downto 0));
+			memOut	:	out	std_logic_vector(34 downto 0));  --get memory content 
 end entity;
 
 
 architecture struct of ConfigAndPassword is
 
-	component MemoryRegister is
+	component MemoryRegister is       --instantiate from memory register
 		port(memIn	:	in	std_logic_vector(34 downto 0);
 				 sel	:	in	std_logic_vector(2 downto 0);
 				 memOut	:	out	std_logic_vector(34 downto 0));
 	end component;
 
-	type t_state is (A, B, C, D);
+	type t_state is (A, B, C, D);  --create fsm with 4 state
 	signal state : t_state := A;
 	signal memIn_tmp, memOut_tmp : std_logic_vector(34 downto 0);
 	signal sel_tmp : std_logic_vector(2 downto 0);
@@ -39,17 +39,17 @@ begin
 
 			elsif(state = B)then
 				if(confirm = '1' and pass = memOut_tmp(34 downto 33))then
-					state <= C;
+					state <= C;   --correct pass entered 
 				else
-					state <= B;
+					state <= B;  -- incorrect pass entered 
 				end if;
 
 			elsif(state = C)then
-				if(confirm = '1')then
+				if(confirm = '1')then -- if 1 pressed goto update state (D)
 					state <= D;
 				end if;
 			
-			elsif(state = D)then
+			elsif(state = D)then  --update system configuration state
 				memIn_tmp <= memIn;
 				sel_tmp <= memSel;
 			end if;
