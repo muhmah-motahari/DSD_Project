0bfe1853d85ae0bc265de4f36aaf9ca6af8501f0
Hami <78771728+MHmi1@users.noreply.github.com>
2023-08-09 20:17:41 +0330

    comments added

--- CoolHeatSystems/CoolHeatSystems.vhdl
+++ CoolHeatSystems/CoolHeatSystems.vhdl
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity CoolHeatSystems is
-	port(chs_conf	:	in	std_logic_vector(7 downto 0);
+	port(chs_conf	:	in	std_logic_vector(7 downto 0);   --input determine mode and power of the system
 			chs_power	:	out	std_logic_vector(3 downto 0);  --adjust power of the fan
 			chs_mode	:	out	std_logic);  -- determine mode of the system 
 end entity;
@@ -11,7 +11,7 @@ end entity;
 architecture struct of CoolHeatSystems is
 	signal count	:	integer	:=	0;
 
-	function parityChecker(s : std_logic_vector) return integer is
+	function parityChecker(s : std_logic_vector) return integer is   --this function return number of ones in input vector 
 	variable temp : natural := 0;
 	begin
 		for i in s'range loop
@@ -22,7 +22,7 @@ architecture struct of CoolHeatSystems is
 		return temp;
 	end function parityChecker;
 
-	function evenParity(num : integer) return std_logic is
+	function evenParity(num : integer) return std_logic is --this function check even parity of the input
 		variable res : std_logic;
 	begin
 		if (num mod 2) = 0 then
@@ -34,7 +34,7 @@ architecture struct of CoolHeatSystems is
 	end function;
 	
 begin 
-	process(chs_conf)
+	process(chs_conf) 
 	begin
 
 		count <= parityChecker(chs_conf);
