78154017a34e3e9b8fa9ee91d4e9e059b742008c
Muhammad Mahdi Motahari <mm.motahari.2001@gmail.com>
2023-08-09 12:21:36 +0330

    Add testbench for MemoryRegister

---
+++ MemoryRegister/testbench.vhdl
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity testbench is
+end entity;
+
+architecture struct of testbench is
+
+	component MemoryRegister is
+		port(memIn	:	in	std_logic_vector(34 downto 0);
+				 sel	:	in	std_logic_vector(2 downto 0);
+				 memOut	:	out	std_logic_vector(34 downto 0));
+	end component;
+
+	signal memIn, memOut : std_logic_vector(34 downto 0) := "00000000000000000000000000000000000";
+	signal sel : std_logic_vector(2 downto 0);
+begin
+
+	UUT: MemoryRegister port map(memIn, sel, memOut);
+
+	memIn <= "00000000000000000000000000011111111", "11000000000000000000000000000000000" after 20 ns;
+	sel <= "001", "111" after 20 ns;
+
+end architecture;
